f23c20addbd194c8f5950c99537760e032911cef
SIVUPE <sivuyisiwepeter79@gmail.com>
2024-03-26 16:22:37 +0200

    fixUp

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -6,10 +6,10 @@
         <img src="https://i.ibb.co/zng7DD6/DSC-0283-1.jpg" loading="lazy" width="250rem" class="img " alt="profile" >
       </div>
       <div class="col-5">
-        <p class="text1 mt-5">
+        <p class="text1 mt-5 text-white">
           Greetings! I'm Sivuyisiwe Peter, a passionate and aspiring Full Stack Developer with a relentless curiosity for technology and a strong desire to create innovative digital solutions. I firmly believe that in today's fast-paced world, technology plays a pivotal role in shaping the future, and I am committed to being a part of that transformation.
         </p>
-        <p class="text mt-5">
+        <p class="text mt-5 text-white">
           MY JOURNEY :
           My journey in the world of development began with a love for problem-solving. I was captivated by the idea of building websites and applications from scratch and understanding how they work. As I delved deeper into this realm, I started to explore various programming languages and frameworks, such as HTML, CSS, JavaScript, React, Node.js, and more. This journey has been both challenging and rewarding, and I thrive on every new challenge that comes my way.
         </p>
@@ -20,6 +20,8 @@
 
 <style >
 body{
-  background-color: #6a9ac4;
+  background-image: radial-gradient(circle at center center, transparent 0%,rgb(0,0,0) 85%),linear-gradient(78deg, rgba(192, 192, 192,0.05) 0%, rgba(192, 192, 192,0.05) 50%,rgba(60, 60, 60,0.05) 50%, rgba(60, 60, 60,0.05) 100%),linear-gradient(227deg, rgba(97, 97, 97,0.05) 0%, rgba(97, 97, 97,0.05) 50%,rgba(52, 52, 52,0.05) 50%, rgba(52, 52, 52,0.05) 100%),linear-gradient(240deg, rgba(98, 98, 98,0.05) 0%, rgba(98, 98, 98,0.05) 50%,rgba(249, 249, 249,0.05) 50%, rgba(249, 249, 249,0.05) 100%),linear-gradient(187deg, rgba(1, 1, 1,0.05) 0%, rgba(1, 1, 1,0.05) 50%,rgba(202, 202, 202,0.05) 50%, rgba(202, 202, 202,0.05) 100%),linear-gradient(101deg, rgba(61, 61, 61,0.05) 0%, rgba(61, 61, 61,0.05) 50%,rgba(254, 254, 254,0.05) 50%, rgba(254, 254, 254,0.05) 100%),linear-gradient(176deg, rgba(237, 237, 237,0.05) 0%, rgba(237, 237, 237,0.05) 50%,rgba(147, 147, 147,0.05) 50%, rgba(147, 147, 147,0.05) 100%),linear-gradient(304deg, rgba(183, 183, 183,0.05) 0%, rgba(183, 183, 183,0.05) 50%,rgba(57, 57, 57,0.05) 50%, rgba(57, 57, 57,0.05) 100%),radial-gradient(circle at center center, hsl(351,4%,12%),hsl(351,4%,12%));
+
+  color: white;
 }
 </style>
